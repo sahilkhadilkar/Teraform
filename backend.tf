ae60b2866de329e8acb801018909a91e628abd05
khads <sahil.khadilkar@in.pega.com>
2021-06-18 20:31:17 +0530

    removing from mastr

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bucket_name"
-    key            = "folder/folder/terraform.tfstate"
-    region         = "region-name"
-    dynamodb_table = "dynamo_db_table_name"
-  }
-}
